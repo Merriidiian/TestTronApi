7e117c65ccaf6037ae9501b20cb8cfc648595104
Merriidiian <iluxa9695@mail.ru>
2023-04-28 00:01:35 +0300

    Refactoring code

--- TronApi/Program.cs
+++ TronApi/Program.cs
@@ -1,78 +1,44 @@
-﻿using System.Text.Json.Nodes;
-using System.Transactions;
-using Newtonsoft.Json;
+﻿using System.Transactions;
 using Newtonsoft.Json.Linq;
 using TronApi.Exceptions;
 using TronApi.Models;
+using TronApi.Services;
 
 public class Program
 {
+    private const int accountLength = 34;
+    private const int badRequest = 400;
+    
+
     public static async Task Main(string[] args)
     {
-        await GetHistoryTransaction();
+        await GetHistoryTransactionAsync();
     }
 
     // 4. Получение истории операций
-    static async Task GetHistoryTransaction()
+    private static async Task GetHistoryTransactionAsync()
     {
-        TransactionInfoModel transactionInfoModel;
-        Console.WriteLine("Ввод адреса");
-        //TNPns1Wa3NZYozYKTJvsEshk6FS4opWgnf
-        string address = Console.ReadLine();
-        if (address.Length != 34)
+        TransactionInfoModel? transactionInfoModel;
+        var response = await new HttpClient().SendAsync(
+            GetHistoryTransactionService.GetHttpConnectionClientAsync(GetHistoryTransactionService
+                .GetAccountAddress()));
+        var dateBegin = GetHistoryTransactionService.GetDataTimeRange("начальной");
+        var dateEnd = GetHistoryTransactionService.GetDataTimeRange("конечной");
+        var messageResponse = response.Content.ReadAsStringAsync();
+        var message = JObject.Parse(messageResponse.Result);
+        if (message.ToObject<ErrorAccountModel>().statusCode == badRequest)
         {
-            throw new AddressFormatException("Неверный формат адреса");
-        }
-        Console.WriteLine("Ввод нижнего прога даты в виде dd.mm.yyyy");
-        string dateBeginConsoleReadLine = Console.ReadLine();
-        DateTime dateBegin = DateTime.Parse(dateBeginConsoleReadLine);
-        if (dateBegin > DateTime.Now)
-        {
-            throw new DataTimeException("Неверно введена дата");
-        }
-        Console.WriteLine("Ввод верхнего прога даты в виде dd.mm.yyyy");
-        string dateEndConsoleReadLine = Console.ReadLine();
-        DateTime dateEnd = DateTime.Parse(dateEndConsoleReadLine);
-        if (dateEnd > DateTime.Now)
-        {
-            throw new DataTimeException("Неверно введена дата");
-        }
-        var client = new HttpClient();
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri($"https://nile.trongrid.io//v1/accounts/{address}/transactions/trc20"),
-            Headers =
-            {
-                { "accept", "application/json" },
-            },
-        };
-        var response = await client.SendAsync(request);
-        var exceptionResponse = response.Content.ReadAsStringAsync();
-        JObject message = JObject.Parse(exceptionResponse.Result);
-        if (message.ToObject<ErrorAccountModel>().statusCode == 400)
-        {
-            throw new AccountException("Акканут не найден");
+            throw new AccountException("Аккаунт не найден");
         }
         transactionInfoModel = message.ToObject<TransactionInfoModel>();
-        for (int i = 0; i < transactionInfoModel.data.Length; i++)
+        foreach (var oneTransaction in transactionInfoModel.data)
         {
-            DateTime date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            date = date.AddSeconds(int.Parse(transactionInfoModel.data[i].block_timestamp.Remove(9, 3)));
-            int resultCompareDataBegin = DateTime.Compare(date, dateBegin);
-            int resultCompareDataEnd = DateTime.Compare(date, dateEnd);
+            var normalDataTime = GetHistoryTransactionService.ParseToNormalDataTimeType(oneTransaction);
+            var resultCompareDataBegin = DateTime.Compare(normalDataTime, dateBegin);
+            var resultCompareDataEnd = DateTime.Compare(normalDataTime, dateEnd);
             if (resultCompareDataBegin >= 0 && resultCompareDataEnd <= 0)
             {
-                Console.WriteLine("ID: " + transactionInfoModel.data[i].transaction_id);
-                Console.WriteLine("DATE: " + date);
-                Console.WriteLine("FROM: " + transactionInfoModel.data[i].from);
-                Console.WriteLine("TO: "+transactionInfoModel.data[i].to);
-                Console.WriteLine("ADDRESS: " + transactionInfoModel.data[i].token_info.address);
-                Console.WriteLine("DECIMALS: " + transactionInfoModel.data[i].token_info.decimals);
-                Console.WriteLine("NAME: " + transactionInfoModel.data[i].token_info.name);
-                Console.WriteLine("SYMBOL: " + transactionInfoModel.data[i].token_info.symbol);
-                Console.WriteLine("TYPE: "+transactionInfoModel.data[i].type);
-                Console.WriteLine("VALUE: "+transactionInfoModel.data[i].value.Remove(transactionInfoModel.data[i].value.Length-6,6));
+                GetHistoryTransactionService.WriteTransactionInfo(oneTransaction);
             }
             else
             {
---
+++ TronApi/Services/GetHistoryTransactionService.cs
@@ -0,0 +1,68 @@
+﻿using TronApi.Exceptions;
+using TronApi.Models;
+
+namespace TronApi.Services;
+
+public static class GetHistoryTransactionService
+{
+    const int accountLength = 34;
+    public static HttpRequestMessage GetHttpConnectionClientAsync(string address)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://nile.trongrid.io//v1/accounts/{address}/transactions/trc20"),
+            Headers =
+            {
+                { "accept", "application/json" },
+            }
+        };
+        return request;
+    }
+
+    public static string GetAccountAddress()
+    {
+        Console.WriteLine("Ввод адреса");
+        //TNPns1Wa3NZYozYKTJvsEshk6FS4opWgnf
+        var address = Console.ReadLine();
+        if (address.Length != accountLength)
+        {
+            throw new AddressFormatException("Неверный формат адреса");
+        }
+
+        return address;
+    }
+
+    public static DateTime GetDataTimeRange(string dateBeginOrEnd)
+    {
+        Console.WriteLine($"Ввод {dateBeginOrEnd} даты в виде dd.mm.yyyy");
+        var dateBeginConsoleReadLine = Console.ReadLine();
+        var date = DateTime.Parse(dateBeginConsoleReadLine);
+        if (date > DateTime.Now)
+        {
+            throw new DataTimeException("Неверно введена дата");
+        }
+        return date;
+    }
+
+    public static DateTime ParseToNormalDataTimeType(Data oneTransaction)
+    {
+        var date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        date = date.AddSeconds(int.Parse(oneTransaction.block_timestamp.Remove(9, 3)));
+        return date;
+    }
+
+    public static void WriteTransactionInfo(Data oneTransaction)
+    {
+        Console.WriteLine("ID: " + oneTransaction.transaction_id);
+        Console.WriteLine("DATE: " + ParseToNormalDataTimeType(oneTransaction));
+        Console.WriteLine("FROM: " + oneTransaction.from);
+        Console.WriteLine("TO: " + oneTransaction.to);
+        Console.WriteLine("ADDRESS: " + oneTransaction.token_info.address);
+        Console.WriteLine("DECIMALS: " + oneTransaction.token_info.decimals);
+        Console.WriteLine("NAME: " + oneTransaction.token_info.name);
+        Console.WriteLine("SYMBOL: " + oneTransaction.token_info.symbol);
+        Console.WriteLine("TYPE: " + oneTransaction.type);
+        Console.WriteLine("VALUE: " + oneTransaction.value.Remove(oneTransaction.value.Length - 6, 6) + " \n");
+    }
+}
